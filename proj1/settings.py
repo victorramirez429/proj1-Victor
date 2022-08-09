36985c25389487835d99f59ed17eb8b39f887d64
victorramirez429 <1219100543@alumnos.utng.edu.mx>
2022-08-09 14:58:40 -0500

    app heroku

--- proj1/settings.py
+++ proj1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5sk#w^tp863)kwwf%jh%g(l=pwlxxbc33*yijhgs4!x80o=i22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'dloredo2.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'appvicautomatiza.herokuapp.com']
 
 
 # Application definition
